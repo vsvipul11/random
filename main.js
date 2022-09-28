d2faf3d6b9ece58b4f812676f1ac3a786748639d
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2022-09-28 16:15:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function getDeviceType() {
     console.log("You are using Mobile or iframe");
     console.log(isIframe);
   } else {
-    window.location.replace(`../../qrcode/index`);
+    window.location.replace(`https://vsvipul11.github.io/random/qrcode/index`);
 
     console.log("You are using Desktop");
   }
